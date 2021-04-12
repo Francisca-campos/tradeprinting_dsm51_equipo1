5d4d9e20cf713fd6c1ccbe4f55535ab2f1c5a3a8
Francisca <al221811678@gmail.com>
2021-04-11 23:04:59 -0500

    autenticacion de login

---
+++ lib/login_admin/contentPage.dart
@@ -0,0 +1,35 @@
+abstract class Content {
+  //Future<HomePageTradeprinting_dsm51_equipo1> lista();
+  //<InicioPage> productos(String id);
+  //Future<MapsPage> mapa();
+  //Future<ListMyrecipes> myrecipe(String id);
+  //Future<InicioPage> admin();
+}
+
+class contentPage implements Content {
+  //Future<HomePageTradeprinting_dsm51_equipo1> lista() async {
+  //return HomePageTradeprinting_dsm51_equipo1();
+  //}
+
+  //Future<MapsPage> mapa() async {
+  //return MapsPage();
+  //}
+
+//  Future<IncioPage> admin() async {
+  //return InicioPage();
+  //}
+
+  //Future<IncioPage> productos(String id) async {
+  //print('en content page $id');
+  //return InicioPage(
+  //id: id,
+  //);
+  //}
+
+  //Future<ListMytradeprinting_dsm51_equipo1> myproducto(String id) async {
+  //  print('listados mis productos $id');
+  // return ListMyTradeprinting_dsm51_equipo1(
+  //   id: id,
+  // );
+  //}
+}
---
+++ lib/login_admin/root_page.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:tradeprinting_dsm51_equipo1/auth/auth.dart';
+
+class RootPage extends StatefulWidget {
+  RootPage({this.auth});
+  final BaseAuth auth;
+  _RootPageState createState() => _RootPageState();
+}
+
+enum AuthStatus { notSignIn, signIn }
+
+class _RootPageState extends State<RootPage> {
+  AuthStatus _authStatus = AuthStatus.notSignIn;
+  @override
+  void initState() {
+    super.initState();
+    widget.auth.currentUser().then((onValue) {
+      setState(() {
+        print(onValue);
+        _authStatus =
+            onValue == 'no_login' ? AuthStatus.notSignIn : AuthStatus.signIn;
+      });
+    });
+  }
+
+  void _signIn() {
+    setState(() {
+      _authStatus = AuthStatus.signIn;
+    });
+  }
+
+  void _signOut() {
+    setState(() {
+      _authStatus = AuthStatus.notSignIn;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget _widget;
+
+    switch (_authStatus) {
+      case AuthStatus.notSignIn:
+        // return IntroScreen(auth: widget.auth,onSignIn: _signIn,);
+        break;
+      case AuthStatus.signIn:
+        //return HomePage( auth: widget.auth,onSignedOut: _signOut,);
+        break;
+    }
+    return _widget;
+  }
+}
